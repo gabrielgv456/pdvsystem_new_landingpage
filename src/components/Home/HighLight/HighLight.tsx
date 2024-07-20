0a188f73025e24ac4689e8242ce6d0e6215311c6
gabriel.vicente@lojasedmil.com.br <gabriel.vicente@lojasedmil.com.br>
2024-07-20 19:05:05 -0300

    fix: acentuacao

--- src/components/Home/HighLight/HighLight.tsx
+++ src/components/Home/HighLight/HighLight.tsx
@@ -8,7 +8,7 @@ export const HighLight = ({ setIsModalRegisterOpen }: modalRegisterType) => {
     return (
         <>
             <S.SectionHighLight>
-                <TitleH1>O sistema que <S.highLightText>descomplica</S.highLightText> a gestão do seu négocio proporcionando eficiência e praticidade em todas as áreas</TitleH1>
+                <TitleH1>O sistema que <S.highLightText>descomplica</S.highLightText> a gestão do seu negócio proporcionando eficiência e praticidade em todas as áreas</TitleH1>
                 <S.SubtitleSection>Controle o estoque, acompanhe graficos, relatórios, realize de vendas, gerencie suas entregas, imprima comprovantes e orçamentos com a personalidade da sua empresa</S.SubtitleSection>
                 <ButtonPrimary onClick={() => setIsModalRegisterOpen(true)}>Comece Agora</ButtonPrimary>
             </S.SectionHighLight>
